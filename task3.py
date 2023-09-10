dee642884e577799d9ce76b2324f7f0dafe6c981
Willmuseve <willmuseve@gmail.com>
2023-09-10 14:37:18 +0300

    task 3 reverse a string

---
+++ task3.py
@@ -0,0 +1,13 @@
+# A python code that prints the reversed version of a string
+# Uppercase and lowercase letters are preserved
+# Prints an empty string
+
+s = "Hello World"
+
+# print(s[::-1])
+
+# reversed = s[::-1]
+# print(reversed)
+
+reversed_word = "".join(reversed(s))
+print(reversed_word)
