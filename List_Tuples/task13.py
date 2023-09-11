1c34b4b7d94544f55a2393fb626cc1d703d4f227
Willmuseve <willmuseve@gmail.com>
2023-09-11 22:38:02 +0300

    All

---
+++ List_Tuples/task13.py
@@ -0,0 +1,8 @@
+# A python program that prints the second smallest value in a list
+# Prints none if the list ios empty
+
+MyList = [21, 9, 21, 10, 27]
+
+if len(MyList) > 1:
+    MyList_sorted = sorted(MyList)
+    print(MyList_sorted[1])
