133dcfa8c4124cb2a07983a28170507123bc2085
Willmuseve <willmuseve@gmail.com>
2023-09-11 17:35:07 +0300

    all

---
+++ List_Tuples/task1.py
@@ -0,0 +1,27 @@
+# A python program that multiplies all items of a list by the value of the variable F
+# prints the list as the output
+# can multiply the value of F by a string is the list contains a string
+# Value of the fctor is a positive integer
+
+#Method 1
+print("Method 1:")
+
+MyList = [2, 3, 4, 5] # initialize the list
+f = 2
+
+for i in range(len(MyList)): #loop throug the elements of the list
+    MyList[i] *= f # multiply through each element by the value of f
+print(MyList, "\n") 
+
+
+
+#Method 2:
+print("Method 2:")
+
+MyList2 = [6, 7, 8, 9]
+g = 3
+
+for i, it in enumerate(MyList2):
+    MyList2[i] = it * g
+
+print(MyList2)
