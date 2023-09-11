a72d278ba92041ffd935b38f125ac6c9647a0161
Willmuseve <willmuseve@gmail.com>
2023-09-11 17:37:51 +0300

    all

--- List_Tuples/task1.py
+++ List_Tuples/task1.py
@@ -1,7 +1,7 @@
 # A python program that multiplies all items of a list by the value of the variable F
 # prints the list as the output
 # can multiply the value of F by a string is the list contains a string
-# Value of the fctor is a positive integer
+# Value of f is a positive integer
 
 #Method 1
 print("Method 1:")
@@ -18,7 +18,7 @@ print(MyList, "\n")
 #Method 2:
 print("Method 2:")
 
-MyList2 = [6, 7, 8, 9]
+MyList2 = ['a', 7, 8, 9]
 g = 3
 
 for i, it in enumerate(MyList2):
