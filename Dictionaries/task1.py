fe71513e07baf8e7628c8a20f56fdf5e92cd2d0f
Willmuseve <willmuseve@gmail.com>
2023-09-12 06:40:19 +0300

    All

---
+++ Dictionaries/task1.py
@@ -0,0 +1,10 @@
+# A python program that checks if a key exists in a dictionary or not
+# If it exists it prints true else prints false
+# Key is stored in a varianble key
+
+MyDict = {"a" : 1, "b" : 2, "c"  : 3, "d" : 4 }
+key = "a"
+key2 = "f"
+
+print(key in MyDict)
+print(key2 in MyDict)
