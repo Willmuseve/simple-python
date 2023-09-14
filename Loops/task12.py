ba331089a8089b684762586bd9ddcfd7f7bd3027
Willmuseve <willmuseve@gmail.com>
2023-09-14 10:33:44 +0300

    Reverse me as a string

---
+++ Loops/task12.py
@@ -0,0 +1,11 @@
+# A program that prints a string reversed in the same line
+
+strings = "HelloWorld"
+strings2 = "Konichiwa"
+
+for char in strings[::-1]:
+    print(char, end = ",")
+
+print("\n")
+for char in reversed(strings2):
+    print(char, end = " ")
