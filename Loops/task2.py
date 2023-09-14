923c96fcdfc8decd77d8466827f1b0d48a415cb8
Willmuseve <willmuseve@gmail.com>
2023-09-14 07:55:38 +0300

    -ve loop

---
+++ Loops/task2.py
@@ -0,0 +1,7 @@
+# A progrm that prints the ints btn a iven number n and 1
+# n is entered by the user and has to be greater or equal to one
+
+n = int(input("Enter number greater than 1: "))
+
+for i in range(n, 0, -1):
+    print(i)
