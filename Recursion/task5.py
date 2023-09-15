586fa9e9a2191baa54941dc514254f83cb286400
Willmuseve <willmuseve@gmail.com>
2023-09-15 13:37:38 +0300

    Recursive power

---
+++ Recursion/task5.py
@@ -0,0 +1,9 @@
+# A program that raises a to power b recursively
+
+def power(a, b):
+    if b == 1:
+        return a
+    else:
+        return a * power(a, b-1)
+    
+print(power(2, 3))
--- Recursion/tempCodeRunnerFile.py
+++
@@ -1,2 +0,0 @@
-
-print(sum_of(345))
